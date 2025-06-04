02c2c117fe720b13ff291094765261443f96fbb8
Isabel <yanxiangw04@gmail.com>
2025-06-04 16:35:53 +0800

    Fix build issues

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -6,7 +6,7 @@ export default [
   ...tseslint.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
-    ignores: ['tailwind.config.ts', 'vite.config.mjs'],
+    ignores: ['tailwind.config.ts', 'vite.config.mjs', 'vitest.config.ts'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
